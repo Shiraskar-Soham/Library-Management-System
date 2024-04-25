2c83789f204a5b1b8f9b540aa0837da02d4538a6
Soham Shiraskar <shiraskarsoham02@gmail.com>
2024-04-25 10:31:22 +0530

    Rental Apis

--- src/main/java/com/example/libraryManagementSystem/repository/RentalRepository.java
+++ src/main/java/com/example/libraryManagementSystem/repository/RentalRepository.java
@@ -2,9 +2,12 @@ package com.example.libraryManagementSystem.repository;
 
 import com.example.libraryManagementSystem.domain.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RentalRepository extends JpaRepository<Rental, Long> {
 
+    @Query(value = "select * from Rentals where returnDate is null and bookId = ?1", nativeQuery = true)
+    Rental findByBookIdAndReturnDateIsNull(String bookId);
 }
--- src/main/java/com/example/libraryManagementSystem/service/RentalService.java
+++ src/main/java/com/example/libraryManagementSystem/service/RentalService.java
@@ -1,18 +1,29 @@
 package com.example.libraryManagementSystem.service;
 
 import com.example.libraryManagementSystem.domain.Rental;
+import com.example.libraryManagementSystem.repository.RentalRepository;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 @Service
 public class RentalService {
+
+    @Autowired
+    private RentalRepository rentalRepository;
+
     public Rental rentBook(Rental r) {
         if (ObjectUtils.isEmpty(r)) throw new RuntimeException("Rental cannot be empty");
-        return null;
+        return rentalRepository.save(r);
     }
 
     public Rental returnBook(String bookId) {
         if (ObjectUtils.isEmpty(bookId)) throw new RuntimeException("Book Id cannot be empty");
-        return null;
+        Rental r = rentalRepository.findByBookIdAndReturnDateIsNull(bookId);
+        if (ObjectUtils.isEmpty(r)) throw new RuntimeException("Book not rented for bookId = " + bookId);
+        Date currentDate = new Date(System.currentTimeMillis());
+        r.setReturnDate(currentDate);
+        return rentalRepository.save(r);
     }
 }
